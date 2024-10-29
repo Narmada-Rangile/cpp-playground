0996eb3d6a6695fad9e5f14fa77280872f4560f3
Devesh <developer.deveshkumar@gmail.com>
2024-10-29 23:20:38 +0530

    initial commit

---
+++ 00 Bite-Sized Lessons/Data Types/data-types.c
@@ -0,0 +1,19 @@
+//
+// Created by devesh on 29/10/24.
+//
+
+#include <stdio.h>
+
+int main() {
+    char var1;               // size: 1 byte, range: -128 to 127
+    unsigned char var2;      // size: 1 byte, range: 0 to 255
+    signed char var3;        // size: 1 byte, range: -128 to 127
+
+    int var4;                // size: 4 bytes, range: -2,147,483,648 to 2,147,483,647
+    unsigned int var5;       // size: 4 bytes, range: 0 to 4,294,967,295
+    signed int var6;         // size: 4 bytes, range: -2,147,483,648 to 2,147,483,647
+
+    short int var7;          // size: 2 bytes, range: -32,768 to 32,767
+    unsigned short int var8; // size: 2 bytes, range: 0 to 65,535
+    signed short int var9;   // size: 2 bytes, range: -32,768 to 32,767
+}
