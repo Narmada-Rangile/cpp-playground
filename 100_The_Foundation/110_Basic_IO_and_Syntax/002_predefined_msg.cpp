c2e04cf80a955a3db592eee1875502951af082f8
devesh <developer.deveshkumar@gmail.com>
2025-07-03 16:31:44 +0530

    add predefined message example in C++

---
+++ 100_The_Foundation/110_Basic_IO_and_Syntax/002_predefined_msg.cpp
@@ -0,0 +1,17 @@
+/*
+This program demonstrates how to declare a string variable with a predefined message
+*/
+
+#include <iostream>
+#include <string>
+
+int main() {
+    // Declare a string variable with a message
+    std::string message = "Hello, this is a predefined message!";
+
+    // Print the contents of the message variable
+    std::cout << message << std::endl;
+
+    return 0;
+}
+
