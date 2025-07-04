4227bf1f0408529ea293bcd816df3c474cbca6f4
devesh <developer.deveshkumar@gmail.com>
2025-07-04 15:54:43 +0530

    add size of data types program

---
+++ 100_The_Foundation/110_Basic_IO_and_Syntax/009_size-of_data-types.cpp
@@ -0,0 +1,26 @@
+// Use the sizeof operator to print the size in bytes of
+// char, short, int, long, long long, float, double, and bool.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    constexpr int char_size = sizeof(char);
+    constexpr int short_size = sizeof(short);
+    constexpr int int_size = sizeof(int);
+    constexpr int long_size = sizeof(long);
+    constexpr int long_long_size = sizeof(long long);
+    constexpr int float_size = sizeof(float);
+    constexpr int double_size = sizeof(double);
+    constexpr int bool_size = sizeof(bool);
+
+    cout << "size of char: " << char_size << endl;
+    cout << "size of short: " << short_size << endl;
+    cout << "size of int: " << int_size << endl;
+    cout << "size of long: " << long_size << endl;
+    cout << "size of long long: " << long_long_size << endl;
+    cout << "size of float: " << float_size << endl;
+    cout << "size of double: " << double_size << endl;
+    cout << "size of bool: " << bool_size << endl;
+}
