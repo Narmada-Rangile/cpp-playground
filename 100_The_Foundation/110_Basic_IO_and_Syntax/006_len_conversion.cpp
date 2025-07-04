8f812121dbc96845ce42d0c687db763074b6b173
Devesh Kumar <developer.deveshkumar@gmail.com>
2025-07-04 14:49:07 +0530

    add length conversion program

---
+++ 100_The_Foundation/110_Basic_IO_and_Syntax/006_len_conversion.cpp
@@ -0,0 +1,37 @@
+// Conversion between lengths in meters and feet.
+
+#include <iostream>
+
+int main()
+{
+    double meter, feet;
+    char choice;
+
+    std::cout << "Length Conversion Program" << std::endl;
+    std::cout << "Choose Conversion Type: \n";
+    std::cout << "1. Meters to Feet\n";
+    std::cout << "2. Feet to Meters\n";
+    std::cout << "Enter Choice: (1 or 2): ";
+    std::cin >> choice;
+
+    if (choice == '1')
+    {
+        std::cout << "Enter Length in Meters: ";
+        std::cin >> meter;
+        feet = meter * 3.28;
+        std::cout << meter << " Meters is " << feet << " feet.\n";
+    }
+    else if (choice == '2')
+    {
+        std::cout << "Enter Length in Feet: ";
+        std::cin >> feet;
+        meter = feet / 3.28;
+        std::cout << feet << " Meters is " << meter << " feet.\n";
+    }
+    else
+    {
+        std::cout << "Invalid Choice. Please run the program again.\n";
+    }
+
+    return 0;
+}
--- README.md
+++ README.md
@@ -53,3 +53,4 @@ Contents is organized on the basis of deepest level of the folder structure.
 3. [Input and Output](100_The_Foundation/110_Basic_IO_and_Syntax/003_input_output.cpp) - A program that takes user input and prints it to the console.
 4. [Sum of Two Numbers](100_The_Foundation/110_Basic_IO_and_Syntax/004_sum.cpp) - A program that takes two numbers as input and prints their sum.
 5. [Temperature Conversion](100_The_Foundation/110_Basic_IO_and_Syntax/005_temp_conversion.cpp) - A program that converts temperature between Celsius and Fahrenheit.
+6. [Length Conversion](100_The_Foundation/110_Basic_IO_and_Syntax/006_len_conversion.cpp) - A program that converts lengths between Meters and Feet.
